446400b1f661a4c6114e59b277db4a8816b2db1d
Steven <na>
2014-12-26 19:24:29 -0600

    finished lesson 1

---
+++ js/resumeBuilder.js
@@ -0,0 +1,31 @@
+var work = [{"employer":"E1",
+                "title":"T1",
+                "location":"L1",
+                "dates":"D1"}];
+var project = [{"title":"T1",
+                "dates":"D1",
+                "description":"D1",
+                "images":"I1"}];
+var bio = {"name":"N1",
+            "role":"R1",
+            "welcomeMessage":"W1",
+            "address":"C1",
+            "skills":["S1","S2"],
+            "contacts":{"mobile":"M1",
+                        "email":"E1"}};
+var education = {};
+
+
+//var formattedName = HTMLheaderName.replace("%data%","Steven");
+//var formattedRole = HTMLheaderRole.replace("%data%","Scientist");
+//$("#header").prepend(formattedName);
+//$("#header").append(formattedRole);
+//
+//var bio = {"name":"Steven","role":"Student","contact":"10001","url":"url","msg":"msg","skills":"skills"};
+//$("#header").append(bio.name,bio.role);
+//$("#header").append(bio["contact"]);
+//
+//var education = {"schools":[
+//    {"name":"S1"},
+//    {"name":"S2"}
+//]};
